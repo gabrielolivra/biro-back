a3cd23a2f081fbc1e79f359e7f10509dd6cca593
gabrielolivra <gabernardinooliveira@gmail.com>
2025-03-09 09:02:28 -0500

    feat: users

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Users } from '../entity/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -16,11 +16,25 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto): Promise<Users> {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const verifyUser = await this.usersRepository.findOne({where: {email: createUserDto.email}})
+    if(verifyUser){
+      throw new BadRequestException('Email already exists')
+    }
+  
     const user = this.usersRepository.create({
       ...createUserDto,
       password: hashedPassword,
     });
-    return this.usersRepository.save(user);
+   this.usersRepository.save(user);
+
+
+   user.password = ''
+   return {
+    ...user,   
+     id: user.id,
+    username: user.username,
+    email: user.email,
+   };
   }
 
   async findOne(username:string){
